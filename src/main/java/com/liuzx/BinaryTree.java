6c409d43bcaa9b10cccdd12d6e215f01c5c9dc82
dream <976633962@qq.com>
2019-08-09 17:26:18 +0800

    中序遍历和后序遍历

--- src/main/java/com/liuzx/BinaryTree.java
+++ src/main/java/com/liuzx/BinaryTree.java
@@ -1,6 +1,7 @@
 package com.liuzx;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
@@ -30,6 +31,54 @@ public class BinaryTree {
             }
             if (currNode.left != null) {
                 stack.push(currNode.left);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * @Author liuzx
+     * @Date 2019/8/9 17:01
+     * @Description 中序排序
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new LinkedList<>();
+        if (root == null) {
+            return list;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            TreeNode currNode = stack.pop();
+            list.add(currNode.val);
+            root = currNode.right;
+        }
+        return list;
+    }
+
+    /**
+     * @Author liuzx
+     * @Date 2019/8/9 17:01
+     * @Description 后续排序
+     */
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new LinkedList<>();
+        if (root == null) {
+            return list;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode currNode = stack.pop();
+            list.add(0, currNode.val);
+            if (currNode.left != null) {
+                stack.push(currNode.left);
+            }
+            if (currNode.right != null) {
+                stack.push(currNode.right);
             }
         }
         return list;
